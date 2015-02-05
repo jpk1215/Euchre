6c0c5de6a7cfd6d1102af6964436ec6aad42fb7a
Jeffrey Klaus <jpk1215@gmail.com>
2015-02-04 19:39:06 -0500

    added gitignore file

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -6,27 +6,12 @@ angular.module('euchreApp',['ngPlayingCards'])
 			{name: "Gabe", hand: []},
 			{name: "David", hand: []}
 		]
-		// $scope.deal = function() {
-		// 	var cards = new Cards();
-		// 	cards.shuffle();
-		// 	for (var i = 0; i <= 1; i++) {
-		// 		for (var j = 0; i < $scope.players.length; i++) {
-		// 			if((i + j) % 2) {
-		// 				$scope.players[j].hand.push(cards.draw());
-		// 				$scope.players[j].hand.push(cards.draw());
-		// 			} else {
-		// 				$scope.players[j].hand.push(cards.draw());
-		// 				$scope.players[j].hand.push(cards.draw());
-		// 				$scope.players[j].hand.push(cards.draw());
-		// 			}
-		// 		};
-		// 	};
-		// }
-		var turnNum = 0;
-		var cards = new Cards();
-		cards.shuffle();
 
 		$scope.deal = function(func){
+			$scope.players = $scope.players.map(function(player){
+				player.hand = [];
+				return player;
+			})
 			var turnNum = 0;
 			var cards = new Cards();
 			cards.shuffle();
@@ -45,6 +30,6 @@ angular.module('euchreApp',['ngPlayingCards'])
 			    turnNum++;
 			    return func;
 			};
-			distibute(distibute());
+			distibute(distibute(console.log(cards)));
 		}
 	})
